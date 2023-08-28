53d026a31413b905e95d3a6c085cab96f51c9da7
razlivaev-dmitriy <142737939+razlivaev-dmitriy@users.noreply.github.com>
2023-08-28 13:46:45 +0300

    Update script.js
    commit1

--- script.js
+++ script.js
@@ -12,8 +12,11 @@ window.onload = function () {
     }
 
     let moveUp = document.querySelector(".moveUp");
-    moveUp.addEventListener("click", function up() {
-        document.documentElement.scrollTop = 0;
+    moveUp.addEventListener("click", function() {
+        document.querySelector("#up").scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
     });
 
     let media_menu = window.matchMedia("(max-width: 992px)");
@@ -62,4 +65,74 @@ window.onload = function () {
     }
     $("#form_but1").on("click", func1);
     $("#form_but2").on("click", func2);
+
+    let show = false;
+    let start;
+    let end;
+
+    function tuda() {
+        anime({
+            targets: ".by_menu",
+            translateX: ["-100%", "-10%"],
+            easing: "easeInOutQuad",
+            duration: 500
+        });
+        anime({
+            targets: ".burger",
+            rotate: 90,
+            duration: 500,
+            easing: "easeInOutQuad"
+        })
+        anime({
+            targets: ".stick",
+            rotate: 180,
+            duration: 500,
+            easing: "easeInOutQuad"
+        })
+        show = true;
+    }
+
+    function ottuda() {
+        anime({
+            targets: ".by_menu",
+            translateX: ["-10%", "-100%"],
+            easing: "easeInOutQuad",
+            duration: 500
+        });
+        anime({
+            targets: ".burger",
+            rotate: 0,
+            duration: 500,
+            easing: "easeInOutQuad"
+        })
+        anime({
+            targets: ".stick",
+            rotate: 0,
+            duration: 500,
+            easing: "easeInOutQuad"
+        })
+        show = false;
+    }
+
+    $(".burger").on("click", function menu() {
+        if (show) {
+            ottuda();
+        } else {
+            tuda();
+        }
+    });
+
+
+    $(document).on("touchstart", function(event) {
+        start = event.originalEvent.touches[0].pageX;
+    })
+
+    $(document).on("touchend", function(event) {
+        end = event.originalEvent.changedTouches[0].pageX;
+        if (end - start >= 100 && !show) {
+            tuda();
+        } else if(start - end >= 100 && show) {
+            ottuda();
+        }
+    })
 }
